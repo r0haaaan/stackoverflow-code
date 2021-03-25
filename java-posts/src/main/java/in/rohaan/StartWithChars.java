95f58986dd0e748f285561f64aedd8a1b362b3e9
Rohan Kumar <rohankanojia420@gmail.com>
2021-03-26 00:12:36 +0530

    Code for https://stackoverflow.com/questions/66795486/

--- java-posts/src/main/java/in/rohaan/StartWithChars.java
+++ java-posts/src/main/java/in/rohaan/StartWithChars.java
@@ -1,2 +1,26 @@
-package in.rohaan;public class StartWithChars {
+package in.rohaan;
+
+import java.util.regex.Pattern;
+
+/*
+ * Code for https://stackoverflow.com/questions/66795486/
+ */  
+public class StartWithChars {
+    public static void main(String[] args) {
+        System.out.println("matches(_FooBar) : " + match("_FooBar"));
+        System.out.println("matches(1FooBar) : " + match("1FooBar"));
+        System.out.println("matches(12FooBar12) : " + match("12FooBar12"));
+        System.out.println("matches(aFooBar) : " + match("aFooBar"));
+        System.out.println("matches($FooBar) : " + match("$FooBar"));
+        System.out.println("matches(-FooBar) : " + match("-FooBar"));
+        System.out.println("matches($$$FooBar) : " + match("$$$FooBar"));
+        System.out.println("matches(12FooBar) : " + match("12FooBar"));
+        System.out.println("matches(12FooBar------) : " + match("12FooBar------"));
+        System.out.println("matches($FooBar_) : " + match("$FooBar_"));
+    }
+
+    private static boolean match(String input) {
+        Pattern p = Pattern.compile("^(\\d|[a-zA-Z$_]).([\\da-zA-Z_])*");
+        return p.matcher(input).matches();
+    }
 }
