ae2b9525156e1368f7db2626fbc4b30f559ace44
Rohan Kumar <rohankanojia420@gmail.com>
2021-04-07 20:06:11 +0530

    Code for https://stackoverflow.com/questions/66986563

--- java-posts/src/main/java/in/rohaan/ReplaceSpecialChar.java
+++ java-posts/src/main/java/in/rohaan/ReplaceSpecialChar.java
@@ -1,2 +1,13 @@
-package in.rohaan;public class ReplaceSpecialChar {
+package in.rohaan;
+
+/*
+ * Code for https://stackoverflow.com/questions/66986563
+ */
+public class ReplaceSpecialChar {
+    public static void main(String[] args) {
+        String input = "bOcfAzr@}Cl~Aa@pO}p@zIgW|TkJjSoW|Ysk@nD}o@zQeRdN_@jAyYvs@{zA~mCce@~x@{\\\\t]gi@jUce@fBmm@gO\\\\";
+        System.out.println("input: " + input);
+        String replaced = input.replace("\\\\", "\\");
+        System.out.println(replaced);
+    }
 }
