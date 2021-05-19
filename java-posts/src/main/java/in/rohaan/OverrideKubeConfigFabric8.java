72385053acf69517fa1a9d803818a81af0a527be
Rohan Kumar <rohankanojia420@gmail.com>
2021-05-19 22:34:37 +0530

    Code for https://stackoverflow.com/questions/67586863

--- java-posts/src/main/java/in/rohaan/OverrideKubeConfigFabric8.java
+++ java-posts/src/main/java/in/rohaan/OverrideKubeConfigFabric8.java
@@ -1,2 +1,21 @@
-package in.rohaan;public class OverrideKubeConfigFabric8 {
+package in.rohaan;
+
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+/*
+ * Code for https://stackoverflow.com/questions/67586863
+ */ 
+public class OverrideKubeConfigFabric8 {
+    public static void main(String[] args) {
+        System.setProperty("kubeconfig", "/tmp/mykubeconfig");
+        try (KubernetesClient kubernetesClient = new DefaultKubernetesClient()) {
+            kubernetesClient.pods().inAnyNamespace().list().getItems().stream()
+                    .map(Pod::getMetadata)
+                    .map(ObjectMeta::getName)
+                    .forEach(System.out::println);
+        }
+    }
 }
