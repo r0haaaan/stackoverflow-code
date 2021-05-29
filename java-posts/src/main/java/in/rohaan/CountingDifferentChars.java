43d3282d80ae6d5a1bf19f8b0c0e01efcb45f311
Rohan Kumar <rohankanojia420@gmail.com>
2021-05-29 21:42:19 +0530

    Code for https://stackoverflow.com/questions/67617176

--- java-posts/src/main/java/in/rohaan/CountingDifferentChars.java
+++ java-posts/src/main/java/in/rohaan/CountingDifferentChars.java
@@ -1,2 +1,34 @@
-package in.rohaan;public class CountingDifferentChars {
+package in.rohaan;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Code for https://stackoverflow.com/questions/67617176
+ */
+public class CountingDifferentChars {
+
+    public static int noOfLetters(String str) {
+        Set<Character> distinctChars = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            boolean isUpperCaseChar = str.charAt(i) >= 'A' && str.charAt(i) <= 'Z';
+            boolean isLowerCaseChar = str.charAt(i) >= 'a' && str.charAt(i) <= 'z';
+            if (isUpperCaseChar || isLowerCaseChar) {
+                char convertedLowerCaseChar = str.charAt(i);
+                if (isUpperCaseChar) {
+                    convertedLowerCaseChar = (char) (str.charAt(i) - 'A' + 'a');
+                }
+                distinctChars.add(convertedLowerCaseChar);
+            }
+        }
+
+        return distinctChars.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(noOfLetters("rohankumar"));
+        System.out.println(noOfLetters("rohankumarROHAN"));
+        System.out.println(noOfLetters("rohankumarROHAN-$"));
+        System.out.println(noOfLetters("cd$aD"));
+    }
 }
